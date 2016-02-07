fd5bda0136e4d28c8fb9e546b4d7a07b81c3c6e0
abunuwas <joseharoperalta@gmail.com>
2016-02-08 00:47:57 +0100

    Include sleepCalendar among the installed apps in settings.py

--- sleepgood/sleepgood/settings.py
+++ sleepgood/sleepgood/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sleepCalendar',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
