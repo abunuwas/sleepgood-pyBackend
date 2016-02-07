f9cc09beb5bb14e87f8ca08e3383aeaa0306f3b2
abunuwas <joseharoperalta@gmail.com>
2016-02-08 00:49:02 +0100

    Define a first index view for the application

--- sleepgood/sleepCalendar/views.py
+++ sleepgood/sleepCalendar/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import uuid
+
+
+def generateUUID(username, date):
+    uuidValue = uuid.uuid3(uuid.NAMESPACE_DNS, username + date)
+    return str(uuidValue)
+
+def indexView(request):
+	return HttpResponse('You are in index view!')
+
