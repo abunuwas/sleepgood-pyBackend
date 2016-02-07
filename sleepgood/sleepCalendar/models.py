463adc55b3d4a71b0b25f0d34582a1c672f4199c
abunuwas <joseharoperalta@gmail.com>
2016-02-08 00:48:29 +0100

    Define a first model for sleepCalendar

--- sleepgood/sleepCalendar/models.py
+++ sleepgood/sleepCalendar/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Calendar(models.Model):
+	sleepingQuality = models.CharField(max_length=7)
+	tirednessFeeling = models.CharField(max_length=7)
+	date = models.DateTimeField()
+	#year = models.IntegerField()
+	#month = models.IntegerField()
+	#day = models.IntegerField()
+	#userId = models.ForeignKey(User, on_delete=models.CASCADE)
+	uuid = models.CharField(max_length=32) # verify that certainly 32 is the max length of an md5 hash
