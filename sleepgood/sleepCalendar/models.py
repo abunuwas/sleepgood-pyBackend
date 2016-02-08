e89cf87d3627025c2f5fffba4bacda13ab7238bd
abunuwas <joseharoperalta@gmail.com>
2016-02-08 01:36:30 +0100

    Provide a string representation of the Calendar model

--- sleepgood/sleepCalendar/models.py
+++ sleepgood/sleepCalendar/models.py
@@ -11,3 +11,7 @@ class Calendar(models.Model):
 	# Provisional field ofr user id until the model for user is implemented
 	userId = models.IntegerField()
 	uuid = models.CharField(max_length=32) # verify that certainly 32 is the max length of an md5 hash
+
+	def __str__(self):
+		return 'Calendar(user={}, date={}, sleepingQuality={}, tirednessFeeling={}, uuid={}'.format(
+			self.userId, self.date, self.sleepingQuality, self.tirednessFeeling, self.uuid)
