6b9999520513125e57682305106c234e8a26e16f
abunuwas <joseharoperalta@gmail.com>
2016-02-08 01:20:14 +0100

    Add provisional field for user id in the Calendar model, and make corresponding migration to update the database

--- sleepgood/sleepCalendar/models.py
+++ sleepgood/sleepCalendar/models.py
@@ -8,4 +8,6 @@ class Calendar(models.Model):
 	#month = models.IntegerField()
 	#day = models.IntegerField()
 	#userId = models.ForeignKey(User, on_delete=models.CASCADE)
+	# Provisional field ofr user id until the model for user is implemented
+	userId = models.IntegerField()
 	uuid = models.CharField(max_length=32) # verify that certainly 32 is the max length of an md5 hash
