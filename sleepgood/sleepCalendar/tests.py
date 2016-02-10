fc604732be31548a5fea34cf117eb6fe81c3f999
abunuwas <joseharoperalta@gmail.com>
2016-02-10 02:49:53 +0100

    Add test for the function insertCalendarEntry()

--- sleepgood/sleepCalendar/tests.py
+++ sleepgood/sleepCalendar/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.http import HttpResponse, HttpRequest
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+import datetime
+
+from .views import insertCalendarEntry
+from .models import Calendar
+
+
+class insertCalendarEntryTest(TestCase):
+
+	def test_entries_inserted_correctly(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['_userId'] = '1'
+		request.POST['sleepingQuality'] = 'Good'
+		request.POST['tirednessFeeling'] = 'Bad'
+		request.POST['_userId'] = '1'
+		request.POST['date'] = '2016-02-09T23:48:14.297Z'
+
+		request_elements = dict(request.POST.items())
+
+		response = insertCalendarEntry(request, 1)
+
+		event = Calendar.objects.all()
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(len(event), 1)
+
+
+
