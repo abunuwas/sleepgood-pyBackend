cc3511725a90e530388ef477f3c597df930c8d8c
abunuwas <joseharoperalta@gmail.com>
2016-02-10 14:08:03 +0100

    Add url mapper for the updateCalendarEntry view

--- sleepgood/sleepCalendar/urls.py
+++ sleepgood/sleepCalendar/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url('^$', views.indexView, name='index'),
     url('^(?P<userId>\d+)/calendar$', views.insertCalendarEntry, name='insertCalendarEntry'), 
     url('(?P<userId>\d+)/calendar/year/(?P<year>\d+)$', views.getCalendarEntriesByYear, name='getCalendarEntriesByYear'),
+    url('^(?P<userId>\d+)/calendar/update$', views.updateCalendarEntry, name='updateCalendarEntry'),
     ]
