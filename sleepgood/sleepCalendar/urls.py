b3232767dcd28e783927bad3ba41e64102425c2b
abunuwas <joseharoperalta@gmail.com>
2016-02-10 12:40:20 +0100

    Map insertCalendaryEntry view to its corresponding url

--- sleepgood/sleepCalendar/urls.py
+++ sleepgood/sleepCalendar/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'sleepCalendar'
 urlpatterns = [
     url('^$', views.indexView, name='index'),
+    url('^(?P<userId>\d+)/calendar$', views.insertCalendarEntry, name='insertCalendarEntry'), 
     url('(?P<userId>\d+)/calendar/year/(?P<year>\d+)$', views.getCalendarEntriesByYear, name='getCalendarEntriesByYear'),
     ]
