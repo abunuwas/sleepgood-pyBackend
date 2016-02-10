a262849a13ee7faac5c83e8e35198dfa5631aea5
abunuwas <joseharoperalta@gmail.com>
2016-02-10 02:51:30 +0100

    Add functions to process the date from the HTML input form and convert it to a format suitable for the database

--- sleepgood/sleepCalendar/views.py
+++ sleepgood/sleepCalendar/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core import serializers
+from django.utils import timezone
 
 import uuid
 import json
+import datetime
 
 from .models import Calendar
 
-
-def generateUUID(username, date):
-    uuidValue = uuid.uuid3(uuid.NAMESPACE_DNS, username + date)
-    return str(uuidValue)
 
 def indexView(request):
 	return HttpResponse('You are in index view!')
@@ -32,14 +30,35 @@ def getCalendarEntriesByYear(request, userId, year):
 	return HttpResponse(data, content_type='application/json')
 	#return HttpResponse('You are in index view!')
 
-b = '''
-{
- "2016-02-02": {
- "ID": "56B0C07ED4C6CD3485EFD066",
- "SLEEPINGQUALITY": "GOOD",
- "TIREDNESSFEELING": "BAD",
- "USERID": 1,
- "DATE": “2016-02-02T00:00:00.000Z"
- “9138C4F6-42DE-4918-89DF-957AF0F6E17D"
- },
-'''
+
+def getYearMonthDayFromISO(dateISO):
+	'''
+	Expects a date in ISO format as returned by the JavaScript function toISOString().
+	For example: 2016-02-09T22:41:21.955Z.
+	'''
+	return dateISO[:10]
+
+def makeDatetimeObject(date):
+	'''Expects a date string in the following format: 2006-12-01'''
+	dateList = date.split('-')
+	dateIntegers = [int(i) for i in dateList]
+	return datetime.datetime(dateIntegers[0], dateIntegers[1], dateIntegers[2])
+
+def getDate(date):
+	'''
+	Helper function that combines the getYearMonthDayFromISO() and the makeDatetimeObject()
+	to provide the date format expected by the database model. 
+	'''
+	date = getYearMonthDayFromISO(date)
+	date = makeDatetimeObject(date)
+	return date
+
+def generateUUID(username, date):
+	'''
+	Returns an md5 hash using string which combines the username plus the calendar date of the event
+	as a way to generate a unique value. Not sure yet, though, if this is the best approach...
+	'''
+	uuidValue = uuid.uuid3(uuid.NAMESPACE_DNS, username + date)
+	return str(uuidValue)
+
+
