d9723f64f157a27be98dc5e25214241174419f5e
abunuwas <joseharoperalta@gmail.com>
2016-02-23 16:43:32 +0100

    Register the accounts app in settings.py and wire up its root url in the project's urls.py module

--- sleepgood/sleepgood/urls.py
+++ sleepgood/sleepgood/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('sleepCalendar.urls', namespace='sleepCalendar')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^admin/', admin.site.urls),
 ]
