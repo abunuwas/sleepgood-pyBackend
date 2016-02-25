4449b13f675195c6a83888032db45af159fa5bd7
abunuwas <joseharoperalta@gmail.com>
2016-02-25 01:29:40 +0100

    Install django-cors-headers to enable communication between the front-end and the back-end applications

--- sleepgood/sleepgood/settings.py
+++ sleepgood/sleepgood/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'sleepCalendar',
     'accounts',
 ]
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,6 +117,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_URLS_REGEX = '' // Specify here which urls are going to be enabled for sending CORS headers.
 
 
 # Static files (CSS, JavaScript, Images)
