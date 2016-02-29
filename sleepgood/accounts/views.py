77f1fd2eb0cb8f1a8cbf1dabba244074adebdd36
abunuwas <joseharoperalta@gmail.com>
2016-02-29 18:26:20 +0100

    Modify views to return HTTP data for experimentation

--- sleepgood/accounts/views.py
+++ sleepgood/accounts/views.py
@@ -5,10 +5,19 @@ from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import get_token
 
 
 class Register(View):
-	http_method_names = ['post']
+	http_method_names = ['post', 'get']
+
+	def get(self, request):
+		get_token(request)
+		data = {}
+		for key, value in dict(request.META).items():
+			data[str(key)] = str(value)
+		return JsonResponse(data)
 
 	def post(self, request):
 		username, email, password = request.POST['username'], request.POST['email'], request.POST['password']
@@ -19,13 +28,16 @@ class Register(View):
 		if user is not None:
 			if user.is_active:
 				login(request, user)
-				return HttpResponse('Good!')
+				data = {}
+				for key, value in dict(request.META).items():
+					data[str(key)] = str(value)
+				return JsonResponse(data)
+				#return HttpResponse('Good!')
 			else: 
 				HttpResponse('You are not active')
 		else:
 			HttpResponse('The account does not exist')
 		
-
 
 @login_required(login_url='/')
 def login(request):
@@ -33,3 +45,15 @@ def login(request):
 
 def logout(request):
 	return HttpResponse('You are in logout') 
+
+def expose(request):
+	get_token(request)
+	#hFwr0EKBGomGwOesoWhQ9FpiMAvKNA5g
+	#csrftoken=hFwr0EKBGomGwOesoWhQ9FpiMAvKNA5g
+	#output = ''
+	#for key, value in dict(request.META).items():
+	#	output += str(key) + ' ========> ' + str(value) + '<br/>'
+	data = {}
+	for key, value in dict(request.META).items():
+		data[str(key)] = str(value)
+	return JsonResponse(data)
