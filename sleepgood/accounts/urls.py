ca705f164d7593b2526eef3c4acd396b6da49f8c
abunuwas <joseharoperalta@gmail.com>
2016-02-29 18:26:53 +0100

    Make the register function csrf exempt

--- sleepgood/accounts/urls.py
+++ sleepgood/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-	url('^register$', views.Register.as_view(), name='register'),
+	url('^register$', csrf_exempt(views.Register.as_view()), name='register'),
 	url('^login$', views.login, name='login'),
 	url('^logout$', views.logout, name='logout'),
+	url('^expose$', views.expose, name='expose'),
 	]
