c4c5d91391bf33b86eb6e9cce0613ca85e26d16b
Vicens Fayos <datracka@gmail.com>
2016-03-02 20:01:17 +0100

    fixed insert and select methods after meeting in balzac

--- sleepgood/sleepCalendar/views.py
+++ sleepgood/sleepCalendar/views.py
@@ -6,6 +6,7 @@ from django.views.generic import View
 from django.core.exceptions import SuspiciousOperation
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 import uuid
 import json
@@ -28,7 +29,7 @@ def getCalendarEntriesByYear(request, year):
 	data = {}
 	for query in queryset:
 		date = query.date
-		date = '{}-{:02d}-{}'.format(date.year, date.month, date.day)
+		date = '{}-{:02d}-{:02d}'.format(date.year, date.month, date.day)
 		data[date] = {'id': query.pk,
 		                    'sleepingQuality': query.sleepingQuality,
 		                    'tirednessFeeling': query.tirednessFeeling,
@@ -66,12 +67,13 @@ def generateUUID(userId, date):
 
 class InsertUpdateDelete(View):
 	http_method_names = ['post', 'put', 'delete']
-
+ 
+	@csrf_exempt
 	def post(self, request):
 		items = dict(request.POST.items())
 		date = getDatetimeFromISO(items['date'])
-		entryUUID = generateUUID(str(items['_userId']), str(date))
-		user = User.objects.get(pk=items['_userId'])
+		entryUUID = generateUUID(str(items['userId']), str(date))
+		user = User.objects.get(pk=items['userId'])
 		newEntry = Calendar(user=user,
 			                date=date,
 			                sleepingQuality=items['sleepingQuality'],
