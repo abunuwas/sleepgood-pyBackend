0dc905622497c4a282951b2a5f35dee8868e8113
abunuwas <joseharoperalta@gmail.com>
2016-03-02 01:30:35 +0100

    Override save() method for Day model to have the uuid automatically generated on save

--- sleepgood/sleepCalendar/models.py
+++ sleepgood/sleepCalendar/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 import uuid
+import datetime
+
+
+def generateUUID(userId, date):
+	'''
+	Returns an md5 hash using string which combines the user id plus the calendar date of the event
+	as a way to generate a unique value. Not sure yet, though, if this is the best approach...
+	'''
+	currentMilliseconds = datetime.datetime.now().timestamp()
+	uuidValue = uuid.uuid3(uuid.NAMESPACE_DNS, str(userId) + str(date) + str(currentMilliseconds))
+	return str(uuidValue)
+
 
 class Day(models.Model):
 	good = 'good'
@@ -14,16 +27,15 @@ class Day(models.Model):
 		)
 	sleepingQuality = models.CharField(max_length=7, choices=choices)
 	tirednessFeeling = models.CharField(max_length=7, choices=choices)
-	day = models.DateTimeField()
+	date = models.DateTimeField()
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	uuid = models.UUIDField(default=uuid.uuid3, editable=False) 
+	uuid = models.CharField(max_length=32)
 	# Verify that auto_now_add and auto_now are actually the best choices for this
 	date_created = models.DateTimeField(auto_now_add=True)
 	date_modified = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return 'Calendar(user={}, date={}, sleepingQuality={}, tirednessFeeling={}, uuid={})'.format(
-			self.user, self.date, self.sleepingQuality, self.tirednessFeeling, self.uuid)
+	class Meta:
+		ordering = ('date_created',)
 
 	def getDict(self):
 		'''
@@ -39,6 +51,14 @@ class Day(models.Model):
 		         'date_modified': str(self.date_modified)
 		         }
 
-	class Meta:
-		ordering = ('date_created',)
+	def save(self, *args, **kwargs):
+		uuid = generateUUID(self.user_id, self.date)
+		self.uuid = str(uuid)
+		super(Day, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return 'Calendar(user={}, date={}, sleepingQuality={}, tirednessFeeling={}, uuid={})'.format(
+			self.user, self.date, self.sleepingQuality, self.tirednessFeeling, self.uuid)
+
+
 
