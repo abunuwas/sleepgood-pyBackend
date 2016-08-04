f18648453e2ee7098df131369019fc4b47573c92
Vicens Fayos <datracka@gmail.com>
2016-08-04 17:21:57 +0200

    refactor and rename Calendar Class names to List and Details

--- sleepgood/sleepCalendar/urls.py
+++ sleepgood/sleepCalendar/urls.py
@@ -1,20 +1,15 @@
 from django.conf.urls import url
-from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework.urlpatterns import format_suffix_patterns 
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 app_name = 'sleepCalendar'
 urlpatterns = [
-    url('^$', views.indexView, name='index'),
-    url('^calendar$', views.InsertUpdateDeleteAPI.as_view(), name='insertCalendarEntry'),
-    url('calendar/uuid/(?P<uuid>[-\w]+)$', views.GetCalendarEntry.as_view(), name='getCalendarEntryByUuid'),
-    url('calendar/year/(?P<date__year>\d+)$', views.GetCalendarEntries.as_view(), name='getCalendarEntriesByYear'),
-    #url('^1/calendar$', csrf_exempt(views.InsertUpdateDelete.as_view()), name='insertCalendarEntry'), 
-    #url('calendar/year/(?P<year>\d+)$', views.getCalendarEntriesByYear, name='getCalendarEntriesByYear'),
-    #url('^(?P<userId>\d+)/calendar/update$', views.InsertUpdate.as_view(), name='updateCalendarEntry'),
-
-    ]
+	url('^$', views.indexView, name='index'),
+	url('^calendar$', views.CalendarDetails.as_view()),
+	url('calendar/uuid/(?P<uuid>[-\w]+)$', views.CalendarDetails.as_view()),
+	url('calendar/year/(?P<date__year>\d+)$', views.CalendarList.as_view())
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- sleepgood/sleepCalendar/views.py
+++ sleepgood/sleepCalendar/views.py
@@ -1,35 +1,19 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
-from django.core import serializers
-from django.utils import timezone
-from django.views.generic import View
-from django.core.exceptions import SuspiciousOperation
-from django.contrib.auth.models import User, Group
-from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
-
-from rest_framework import viewsets, status, mixins, generics, permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 import json
-import datetime
-import dateutil.parser
 
+from django.contrib.auth.models import User, Group
+from django.http import HttpResponse
+from rest_framework import viewsets, status, mixins, generics, permissions
+from rest_framework.response import Response
 from .models import Day
-from .serializers import UserSerializer, GroupSerializer, DaySerializer, EntrySerializer
 from .permissions import IsOwnerOrReadOnly
+from .serializers import UserSerializer, GroupSerializer, DaySerializer, EntrySerializer
 
 
 def indexView(request):
 	return HttpResponse('You are in index view!')
 
-#######################################################################
-
-
-class GetCalendarEntries(mixins.ListModelMixin,
-						generics.GenericAPIView):
+class CalendarList(mixins.ListModelMixin,
+				   generics.GenericAPIView):
 	queryset = Day.objects.all()
 	lookup_field = 'date__year'
 	serializer_class = DaySerializer
@@ -41,24 +25,28 @@ class GetCalendarEntries(mixins.ListModelMixin,
 		user = request.user
 		userId = user.id
 		print(userId);
-		# Filter data by user and year. Maybe this should be modified later on... 
+		# Filter data by user and year. Maybe this should be modified later on...
 		queryset = Day.objects.filter(user=userId, date__year=year)
 		serializer = self.get_serializer(queryset, many=True)
 		return_data = {}
 		for result in serializer.data:
 			# Take the date year-month-day data from the ISO structure
-			# to use it as a key for the data related to this date in the 
-			# JSON. 
+			# to use it as a key for the data related to this date in the
+			# JSON.
 			date = result['date'][:10]
-			return_data[date] = result	    
+			return_data[date] = result
 		return Response(return_data)
 
 	def get(self, request, *args, **kwargs):
 		result_set = self.list(request, *args, **kwargs)
 		return result_set
 
-class GetCalendarEntry(mixins.ListModelMixin,
-						generics.GenericAPIView):
+
+class CalendarDetails(mixins.RetrieveModelMixin,
+					  mixins.CreateModelMixin,
+					  mixins.UpdateModelMixin,
+					  mixins.DestroyModelMixin,
+					  generics.GenericAPIView):
 	queryset = Day.objects.all()
 	lookup_field = 'uuid'
 	serializer_class = EntrySerializer
@@ -73,14 +61,14 @@ class GetCalendarEntry(mixins.ListModelMixin,
 		serializer = self.get_serializer(queryset, many=True)
 		return Response(serializer.data)
 
-class InsertUpdateDeleteAPI(mixins.RetrieveModelMixin,
-							mixins.CreateModelMixin,
-							mixins.UpdateModelMixin,
-							mixins.DestroyModelMixin,
-							generics.GenericAPIView):
-	serializer_class = DaySerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-						  IsOwnerOrReadOnly,)
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+	def put(self, request, *args, **kwargs):
+		return self.update(request, *args, **kwargs)
+
+	def delete(self, request, *args, **kwargs):
+		return self.destroy(request, *args, **kwargs)
 
 	def create(self, request, *args, **kwargs):
 		# SUPER IMPORTANT! CHECK FIRST THAT THE DAY HASN'T ALREADY BEEN SAVED IN THE DB.
@@ -95,43 +83,43 @@ class InsertUpdateDeleteAPI(mixins.RetrieveModelMixin,
 
 	def perform_create(self, serializer):
 		# Override this method passing user data to the save method, which will signed
-		# the current user as owner of this data. 
+		# the current user as owner of this data.
 		serializer.save(user=self.request.user)
 
 	def update(self, request, *args, **kwargs):
 		'''
-		It updates a value in the database. Data can only be modified by users who own/created it. 
-		This method extensively overrides the original update method from the UpdateModelMixin class. 
+		It updates a value in the database. Data can only be modified by users who own/created it.
+		This method extensively overrides the original update method from the UpdateModelMixin class.
 		It expects the following parameters from the request:
 		<uuid>
 		<tirednessFeeling>
 		<sleepingQuality>
-		User data must be included in the headers. 
+		User data must be included in the headers.
 		'''
-		# Create dictionary of values from payload in the request. 
+		# Create dictionary of values from payload in the request.
 		# The `values` dictionary stores data that will be used to build a
-		# serializer with which we can updated the corresponding database entry. 
+		# serializer with which we can updated the corresponding database entry.
 		values = {key: value for key, value in self.request.data.items()}
-		# Obtain the primary key of the user object returned by the request. 
+		# Obtain the primary key of the user object returned by the request.
 		values['user'] = self.request.user.pk
-		# Retrieve database entry corresponding to the uuid value included in the request. 
+		# Retrieve database entry corresponding to the uuid value included in the request.
 		dbEntry = Day.objects.get(uuid=values['uuid'])
-		# Include the date field from the database entry in the values dictioanry. 		
+		# Include the date field from the database entry in the values dictioanry.
 		values['date'] = dbEntry.date
-		# Build a dictionary of values for the serializer object from the database entry. 
+		# Build a dictionary of values for the serializer object from the database entry.
 		serializer_values = dbEntry.getDict()
-		# Add the primary of hte user to the dictionary of values for the seriazlier. 
+		# Add the primary of hte user to the dictionary of values for the seriazlier.
 		serializer_values['user'] = self.request.user.pk
 		# Build the serializer object. This object represents the old database entry
-		# that will be updated. 
+		# that will be updated.
 		serializer = DaySerializer(data=serializer_values)
-		# Validate data in the serializer to be able to save it. 
+		# Validate data in the serializer to be able to save it.
 		serializer.is_valid(raise_exception=True)
-		# Build new serializer from the new values included in the request. 
+		# Build new serializer from the new values included in the request.
 		newSerializer = DaySerializer(dbEntry, data=values)
-		# Validate data for the new serializer object. 
+		# Validate data for the new serializer object.
 		newSerializer.is_valid(raise_exception=True)
-		# Update database entry. 
+		# Update database entry.
 		self.perform_update(newSerializer)
 		return Response(newSerializer.data)
 
@@ -141,16 +129,6 @@ class InsertUpdateDeleteAPI(mixins.RetrieveModelMixin,
 		return_data = json.dumps(return_data)
 		self.perform_destroy(dbEntry)
 		return Response(return_data)
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
-
 
 
 #######################################################################
@@ -162,11 +140,10 @@ class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
 
+
 class GroupViewSet(viewsets.ModelViewSet):
 	'''
 	API endpoint that allows groups to be viewed or edied.
 	'''
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
-
-
